709b58d50f28df917ebc4ba057c99e5f5568d278
Alex Waterman <imNotListening@gmail.com>
2012-07-15 17:25:44 -0400

    First commit for Dragon Linux
    Dragon Linux will be a source based Linux distro aimed at being as small
    and lightweight as possible.
    
    Signed-off-by: Alex Waterman <imNotListening@gmail.com>

---
+++ build/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Build the dragon linux source into a workable image.
+#
+
+# Check to see if we have an architecture.
+if [ ! $ARCH ]; then
+    ARCH=`uname -m`
+    echo Setting default arch: $ARCH
+fi
+
